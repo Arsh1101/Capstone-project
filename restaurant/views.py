9500e73af84528b438e709e2cce6f0c23beef220
arshh1101 <ald.arshia@gmail.com>
2023-10-17 22:28:07 -0700

    Added the API modules.

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializer import MenuSerializer
+from .models import Menu
 
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SingleMenuItemView(generics.RetrieveAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
